0d54ec4b095e02fd6fa36194f1816a34ca7e4bee
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2021-10-05 21:12:41 +0100

    updated

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -2,6 +2,36 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+const localRestaurants = [
+  {
+  name: "Beachside Bar",
+  image_url:
+    "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  categories: ["Cafe", "Bar"],
+  price: "$$",
+  reviews: 1244,
+  rating: 4.5,
+},
+{
+  name: "Benihana",
+  image_url:
+    "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  categories: ["Cafe", "Bar"],
+  price: "$$",
+  reviews: 1244,
+  rating: 3.7,
+},
+{
+  name: "India's Grill",
+  image_url:
+    "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  categories: ["Indian", "Bar"],
+  price: "$$",
+  reviews: 700,
+  rating: 4.9,
+},
+]
+
 const RestaurantImage = () => (
   <>
     <Image
@@ -47,10 +77,13 @@ const RestaurantInfo = () => (
 const RestaurantItem = () => {
   return (
     <TouchableOpacity activeOpacity={0.8} style={{ marginBottom: 30 }}>
-      <View style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}>
-        <RestaurantImage />
-        <RestaurantInfo />
-      </View>
+      {localRestaurants.map((restaurant, index) => (
+           <View key={index} style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}>
+           <RestaurantImage />
+           <RestaurantInfo />
+         </View>
+      ))}
+     
     </TouchableOpacity>
   );
 };
